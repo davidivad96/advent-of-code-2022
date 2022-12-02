35eea5d07e83e79e7297a066fbd0f23c0f98d4ed
davidivad96 <david@strivelabs.io>
2022-12-02 13:18:38 +0100

    improve "indexFileTemplate"

--- src/cli/utils.ts
+++ src/cli/utils.ts
@@ -3,8 +3,11 @@ import { writeFileSync } from "fs";
 const indexFileTemplate = `import { partOne } from "./partOne";
 import { partTwo } from "./partTwo";
 
+const preprocess = (text: string) =>
+  text.split("\\n").map((line) => parseInt(line));
+
 export const runSolution = (text: string, isPartTwo: boolean) => {
-  const input = text.split("\\n").map((line) => parseInt(line));
+  const input = preprocess(text);
   return isPartTwo ? partTwo(input) : partOne(input);
 };
 `;
