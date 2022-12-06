76b98c08f3f7cf8b63b4b6e44af01d8f168c679b
davidivad96 <david@strivelabs.io>
2022-12-06 15:15:58 +0100

    refactor code

--- src/solutions/06/partOne.ts
+++ src/solutions/06/partOne.ts
@@ -1,7 +1,7 @@
 export const partOne = (input: string) => {
   const N = 4;
   for (let i = 0; i < input.length; i++) {
-    const characters = Array.from({ length: N }, (_, j) => input[i + j]);
+    const characters = input.split("").slice(i, i + N);
     const uniqueCharacters = new Set(characters);
     if (uniqueCharacters.size === N) {
       return i + N;
--- src/solutions/06/partTwo.ts
+++ src/solutions/06/partTwo.ts
@@ -1,7 +1,7 @@
 export const partTwo = (input: string) => {
   const N = 14;
   for (let i = 0; i < input.length; i++) {
-    const characters = Array.from({ length: N }, (_, j) => input[i + j]);
+    const characters = input.split("").slice(i, i + N);
     const uniqueCharacters = new Set(characters);
     if (uniqueCharacters.size === N) {
       return i + N;
